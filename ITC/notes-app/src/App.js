f12168cb01ccfc64feb5595d500edaab023e9316
benjaminmiharris <benjmiharris@gmail.com>
2022-11-07 18:34:28 +0200

    commit tester

--- ITC/notes-app/src/App.js
+++ ITC/notes-app/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+
+import Stack from 'react-bootstrap/Stack';
+
+function HorizontalExample() {
+  return (
+    <Stack direction="horizontal" gap={3}>
+      <div className="bg-light border ms-auto">First item</div>
+      <div className="bg-light border ms-auto">Second item</div>
+      <div className="bg-light border ms-auto">Third item</div>
+    </Stack>
+  );
+}
+
+
+function ButtonExample() {
+  return (
+    <Button variant="primary">
+      Profile <Badge bg="secondary">9</Badge>
+      <span className="visually-hidden">unread messages</span>
+    </Button>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      < ButtonExample />
+      < HorizontalExample />
     </div>
   );
 }
